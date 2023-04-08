b314e192d6d442be19db5b0726d2f2ee27546739
me-sajib <me.mrsajib@gmail.com>
2023-04-08 22:39:55 +0600

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import FlashSaleProduct from "./components/FlashSaleProduct";
+import TrendingProduct from "./components/TrendingProduct";
+
+const App = () => {
+  return (
+    <main>
+      <FlashSaleProduct />
+      <TrendingProduct />
+    </main>
+  );
+};
+
+export default App;
